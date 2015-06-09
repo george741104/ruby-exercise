5ecd1f0e9c842b52a4849f01c10c8c4839fe2840
George Chang <georgec1104@gmail.com>
2015-06-09 17:03:46 +0800

    finished class 1

---
+++ class1.rb
@@ -0,0 +1,32 @@
+class MyCar
+      def initialize (year, model, color)
+            @year = year
+            @model = model
+            @color = color
+            @current_speed = 0
+      end
+      def speed_up(number)
+            @current_speed += number
+      end
+      def brake(number)
+            @current_speed -= number
+      end
+      def current_speed
+            puts "#{@current_speed} kph."
+      end
+      def shut_down
+            @current_speed = 0
+            puts"it's off."
+      end
+end
+
+
+nissan = MyCar.new(2011, "Teana", "black")
+nissan.speed_up(180)
+nissan.current_speed
+nissan.speed_up(25)
+nissan.current_speed
+nissan.brake(80)
+nissan.current_speed
+nissan.shut_down
+nissan.current_speed
