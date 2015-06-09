0478601b1251993b04df4e55b641f97a75735cbc
George Chang <georgec1104@gmail.com>
2015-06-09 11:46:43 +0800

    finished basic 3

---
+++ basic3.rb
@@ -0,0 +1,13 @@
+movies = {
+      a: 1911,
+      b: 1854,
+      c: 1233,
+      d: 1048,
+      e: 1344
+}
+
+puts movies [:a]
+puts movies [:b]
+puts movies [:c]
+puts movies [:d]
+puts movies [:e]
