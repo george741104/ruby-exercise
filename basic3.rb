7f5e87749b4f10a1c12db4994a7628eb2ea0fd68
George Chang <georgec1104@gmail.com>
2015-06-09 12:00:25 +0800

    finished basic 4

--- basic3.rb
+++ basic3.rb
@@ -11,3 +11,28 @@ puts movies [:b]
 puts movies [:c]
 puts movies [:d]
 puts movies [:e]
+
+
+
+arr = [movies [:a],
+         movies [:b],
+         movies [:c],
+         movies [:d],
+         movies [:e]   ]
+
+puts arr
+
+
+# def printing
+#       i = 0
+
+#       while i < Arr.length
+#             puts a[i]
+#             i+=1
+#       end
+
+#  end
+
+# printing
+
+
