0e97ed692985b7b8b033b78a07a805cee1b14e10
George Chang <georgec1104@gmail.com>
2015-06-09 12:13:02 +0800

    finished flow control 2

---
+++ flowcontrol1.rb
@@ -0,0 +1,12 @@
+def cap (string)
+      if string.length > 10
+            string.upcase
+      else
+            string
+      end
+
+end
+
+puts cap ("jordan")
+
+puts cap ("This is a beautiful day!")
